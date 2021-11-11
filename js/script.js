35980abf1db83d03cfce9158f691f0ef89ae8a1f
wnsdnn <a24369029@gmail.com>
2021-11-11 10:08:44 +0900

    완성

--- js/script.js
+++ js/script.js
@@ -19,6 +19,8 @@ const data5 = data[4];   // data에 5번째 데이터
 
 
 const paper = document.querySelectorAll(".paper .paper_box");
+const useCount = document.querySelector(".text span");
+let coloredPaperCount = 0
 
 // 채워져 있는 칸의 번호가 있는 배열
 let dataIdxList = [];
@@ -34,99 +36,118 @@ let paperList = [];
 // ]
 
 
-const setData = (arr) => {
-    arr.forEach((ele, idx) => {
-        widthCount = 0
-        heightCount = 0
-        widthVel = 0
-        heightVel = 0
-        for(let i = 0; i < 5; i++){
-            if(arr.includes(ele + i)){
-                if(widthVel === i){
-                    widthVel = i + 1
-                    widthCount += 1
-                }
-            }
-            if(arr.includes(ele + i*10)){
-                if(heightVel === i){
-                    heightVel = i + 1
-                    heightCount += 1
-                }
+// json형식의 배열을 만들어주는 함수
+const setData = (arr, ele) => {
+    // 색종이가 가로, 세로 연속되어 붙어있는지 판단할 count 변수 
+    let widthCount = 0;
+    let heightCount = 0;
+    // 가로, 세로의 값을 담을 변수들
+    let widthVel = 0;
+    let heightVel = 0;
+
+    for(let i = 0; i < 5; i++){
+        if(arr.includes(ele + i)){
+            if(widthVel === i){
+                widthVel = i + 1;
+                widthCount += 1;
             }
         }
-        size = widthCount > heightCount ? heightCount : widthCount
-        paperList.push({id: ele, width_len: widthCount, height_len: heightCount, size})
-    })
+        if(arr.includes(ele + i*10)){
+            if(heightVel === i){
+                heightVel = i + 1;
+                heightCount += 1;
+            }
+        }
+    }
+    size = widthCount > heightCount ? heightCount : widthCount;
+    return {id: ele, width_len: widthCount, height_len: heightCount, size}
 }
 
-
+// 데이터를 걸려내는 함수
 const dataFilter = (string) => {
     const str = string.replace(/\s*/g, "");
     const arr = str.split("");
-    
 
+    // 해당 배열의 값이 1이면 그 해당배열 인덱스 + 1의 값을 배열에 담기
     arr.filter((ele, idx) => { 
-        ele === "1" ? dataIdxList.push(idx+1) : false 
+        ele === "1" ? dataIdxList.push(idx+1) : false ;
+        // if(ele === "1"){
+        //     paper[idx].style.background = "#000"
+        // }
     });
 
-    setData(arr)
+    // 배열을 json형식의 배열로 만들기
+    dataIdxList.forEach((ele, idx) => {
+        paperList.push(setData(dataIdxList, ele)) 
+    })
 };
 
 
-
+// 색을 칠해주는 함수
 const fillBlock = () => {
     // 배열 size 내림차순으로 재정의
     paperList = paperList.sort((a, b) => {
         return b.size - a.size;
     })
 
-    delNumList = []
-    console.log(paperList);
-    console.log(dataIdxList);
+    // paperList에 0번째 값이 없을때까지 while문 반복
+    while(paperList[0]){
+        count = 0;                  // 배열에 해당 번호가 있으면 카운트 +1
+        numList = [];               // 색종이의 위치를 담을 배열
+        size = paperList[0].size;   // 0번째에 들어올 색종이의 크기
+        paperIdxList = []           // 색종이를 지울때 사용할 색종이들의 id값이 담긴 배열
 
-    for(const ele of paperList){
-        console.log(ele );
-        let bool = false
-        for(const data of delNumList){
-            if(data === ele.id){
-                bool = true
-                // console.log(data);
-            }
+        // paperIdxList에 색종이들의 id 담기
+        for(let i = 0; i < paperList.length; i++){
+            paperIdxList.push(paperList[i].id)
         }
-        if(bool){continue}
-        count = 0
-        numList = []
-        size = ele.size
-        for(let i = 0; i < size; i ++){
+
+        // 해당 배열에 색종이가 있으면 count +1
+        for(let i = 0; i < size; i++){
             for(let j = 0; j < size; j++){
-                num = ele.id + i*10 + j
-                // console.log(num);
-                numList.push(num)
-                if(dataIdxList.includes(num)){count += 1}
+                num = paperList[0].id + i*10 + j;
+                numList.push(num);
+                if(paperIdxList.includes(num)){ count+=1 }
             }
         }
-        if(count === numList.length){
-            thisColor = color[size - 1]
-            delNumList = [...delNumList, ...numList]
+        
+        // 색종이가 있을 위치에 색종이가 다 채워져 있으면 실행
+        if(count == numList.length){
+            coloredPaperCount += 1
+            colorpaper[size-1] = colorpaper[size-1] - 1
+            // 색종이가 모자랄때에 설명은 없어서 아직 안만듬
+
+            thisColor = color[size - 1];
             for(let i = 0; i < numList.length; i++){
-                for(let j = 0; j < dataIdxList.length; j++){
-                    if(numList[i] == dataIdxList[j]){
-                        dataIdxList.splice(j, 1)
+                for(let j = 0; j < paperList.length; j ++){
+                    if(numList[i] === paperList[j].id){
+                        paperList.splice(j, 1)
+                        paperIdxList.splice(j, 1)
                     }
                 }
                 paper[numList[i] - 1].style.background = `${thisColor}`;
             }
+
+            // 값 초기화
+            paperList = []
+            // 배열 재정의
+            paperIdxList.forEach((ele, idx) => {
+                paperList.push(setData(paperIdxList, ele)) 
+            })
         }
     }
+
+    useCount.innerHTML = coloredPaperCount
+    // console.log(colorpaper);
 
 }
 
 
 
-dataFilter( data1 );
+// dataFilter( data1 );
 // dataFilter( data2 );
 // dataFilter( data3 );
-// dataFilter( data4 );
+dataFilter( data4 );
 // dataFilter( data5 );
 
 fillBlock()
--- js/test.js
+++ js/test.js
@@ -1,9 +1,7 @@
 let arr = [12, 13, 22, 23, 24, 33, 34, 35, 36, 37, 44, 45, 46, 47, 55, 56, 57, 63]
 
-for(let i = 0; i<arr.length; i++){
-    if(arr[i] == 34){
-        arr.splice(i, 1)
-    }
-}
 
-console.log(arr);
+while(arr[0]){
+    console.log(arr);
+    arr.splice(0, 1)
+}
